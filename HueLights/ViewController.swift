2260783a0eafc09b8be62f3aef2357221bed88bc
Sam Wu <samuel.wu@mlb.com>
2017-04-22 13:39:55 -0400

    Initial Commit

---
+++ HueLights/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HueLights
+//
+//  Created by Wu, Samuel on 4/22/17.
+//  Copyright © 2017 BothPoints. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+

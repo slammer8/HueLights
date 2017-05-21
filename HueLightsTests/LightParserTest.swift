509b3b6ea52f7555bf6590ff667e726ffebc124f
Sam Wu <samuel.wu@mlb.com>
2017-05-21 17:26:21 -0400

    Finish adding tests

--- HueLightsTests/LightParserTest.swift
+++ HueLightsTests/LightParserTest.swift
@@ -11,17 +11,44 @@ import XCTest
 
 class LightParserTest: XCTestCase {
 
-    func testParser() {
-      let lightsJSON = JSONLoader.load(file: "SampleResponse")
+  func testParser() {
+    let lightsJSON = JSONLoader.load(file: "SampleResponse")
 
-      XCTAssertNotNil(lightsJSON, "JSON should not be nil")
+    XCTAssertNotNil(lightsJSON, "JSON should not be nil")
+
+    let lights = LightParser.parse(jsonString: lightsJSON)
+
+    XCTAssertEqual(lights.count, 3, "There should be 3 lights")
+
+    let firstLight = lights.filter { $0.lightName == "1"}.first
+    let lastLight = lights.filter { $0.lightName == "3"}.first
+
+    if let firstLight = firstLight{
+      testFirstLight(light: firstLight)
+    } else {
+      XCTFail("Unable to load first light")
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+
+    if let lastLight = lastLight {
+      testLastLight(light: lastLight)
+    } else {
+      XCTFail("Unable to load first light")
     }
-    
+
+  }
+
+
+  func testFirstLight(light: Light) {
+    XCTAssertEqual(light.isOn, true, "Light should be on")
+    XCTAssertEqual(light.brightness, 146, "Brightness not correct")
+    XCTAssertEqual(light.hue, 13234, "Hue not correct")
+    XCTAssertEqual(light.saturation, 208, "Saturation not correct")
+  }
+
+  func testLastLight(light: Light) {
+    XCTAssertEqual(light.isOn, false, "Light should be off")
+    XCTAssertEqual(light.brightness, 100, "Brightness not correct")
+    XCTAssertEqual(light.hue, 13122, "Hue not correct")
+    XCTAssertEqual(light.saturation, 211, "Saturation not correct")
+  }
 }
